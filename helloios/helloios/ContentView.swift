61644c7737a1dc9a3c18d96976327aaffb1258d6
haeun <haeuni437@gmail.com>
2024-03-27 20:28:42 -0700

    helloios

---
+++ helloios/helloios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  helloios
+//
+//  Created by 이하은 on 3/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Go Seahawks!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ helloios/helloios/helloiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloiosApp.swift
+//  helloios
+//
+//  Created by 이하은 on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct helloiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
